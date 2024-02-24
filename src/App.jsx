5cbca1a320bebcd80ffaa4e44e23bd03b1f324ea
Josh Wedekind <josh@halfnibble.com>
2024-02-24 12:55:01 -0800

    Example api-hello-world UI component

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { AppShell, Burger, Title, Table } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const App = () => {
+    const [opened, { toggle }] = useDisclosure();
+    const [languages, setLanguages] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3005/languages').then((response) => {
+            setLanguages(response.data);
+        });
+    });
+
+    // Function to render table rows
+    const rows = languages.map((row, index) => (
+        <Table.Tr key={index}>
+            <Table.Td>{row.name}</Table.Td>
+            <Table.Td>{row.greeting}</Table.Td>
+            <Table.Td>{row.pangram}</Table.Td>
+            <Table.Td>{row.filler}</Table.Td>
+        </Table.Tr>
+    ));
+
+    return (
+        <>
+            <AppShell
+                header={{ height: 60 }}
+                navbar={{
+                    width: 300,
+                    breakpoint: 'sm',
+                    collapsed: { mobile: !opened },
+                }}
+                padding='md'
+            >
+                <AppShell.Header>
+                    <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+                    <Title order={1} style={{ marginLeft: '16px' }}>
+                        Hello World API
+                    </Title>
+                </AppShell.Header>
+
+                <AppShell.Navbar p='md'>
+                    <Title order={2}>Languages</Title>
+                </AppShell.Navbar>
+
+                <AppShell.Main>
+                    <Title order={2}>Languages List</Title>
+                    <Table>
+                        <Table.Thead>
+                            <Table.Tr>
+                                <Table.Th>Name</Table.Th>
+                                <Table.Th>Greeting</Table.Th>
+                                <Table.Th>Pangram</Table.Th>
+                                <Table.Th>Filler</Table.Th>
+                            </Table.Tr>
+                        </Table.Thead>
+                        <Table.Tbody>{rows}</Table.Tbody>
+                    </Table>
+                </AppShell.Main>
+            </AppShell>
+        </>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import { createTheme, MantineProvider } from '@mantine/core';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <MantineProvider defaultColorScheme='dark' theme={theme}>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
